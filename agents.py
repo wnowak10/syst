3d721d6c2ac527b5a10173b06687cdc01fc3eb8f
wnowak10 <wnowak10@gmail.com>
2018-10-09 11:34:13 -0400

    moved families out so new families generated each time step.

--- agents.py
+++ agents.py
@@ -9,13 +9,14 @@ from mesa.time import RandomActivation
 # Constants 
 # ______________________________________________________________________________
 
-SCANDAL_PROBABILITY = .05
-ENDOWMENT_GROWTH_RATE = .04 
+SCANDAL_PROBABILITY   = .03 # Use Choate as representative -- ~3/125 ~~ 3%
+ENDOWMENT_GROWTH_RATE = .04 # Standard market returns. 
 
 
 class FamilyAgent(Agent):
-    """ An family contains the following parameters:
+    """A family who chooses to attend a given school.
 
+    A family contains the following parameters:
     - Wealth
     - Location (where they live on a 1 dimensional space)
     - Weighting of their priorities in choosing a school
@@ -23,50 +24,57 @@ class FamilyAgent(Agent):
     def __init__(self, unique_id, model, 
                 # Custom parameters
                 wealth, 
-                priority,
+                priorities,
                 location):
         super().__init__(unique_id, model)
-        self.wealth   = wealth
-        self.location = location
-        self.priority = priority
+        self.wealth     = wealth
+        self.location   = location
+        self.priorities = priorities
 
     def score_school(self, school):
         """ Given a school and a Family's parameters, return the utility score for that school.
         """
-        print(self.priority)
-        print(self.unique_id)
-        print('prestige', school.prestige)
-        print('endowment', school.endowment)
-        if self.priority == 'prestige':
-            return school.prestige
-        else:
-            return school.endowment
+        score = (self.priorities['Prestige']*school.prestige +
+        	     self.priorities['Efficacy']*school.efficacy - 
+        	     self.priorities['Cost']*school.tuition)
+        # print('priorities for familiy', self.unique_id, self.priorities['Prestige'], self.priorities['Efficacy'])
+        return score
 
-    def school_choice(self, wealth, location, priority):
+    def school_choice(self, wealth, location, priorities):
         """ Score each school using `score_school` logic.
         Then return the id of chosen school. 
         """
         schools= [obj for obj in self.model.schedule._agents.values() if isinstance(obj, SchoolAgent)]
         scores = [self.score_school(school) for school in schools]
         chosen_school = np.argmax(scores)
-        print('chose school is :', chosen_school)
+        # print('Family {} chose school {}'.format(str(self.unique_id), str(chosen_school)))
         return chosen_school
 
     def step(self):
-        chosen_school = self.school_choice(self.wealth, self.location, self.priority)
+        chosen_school = self.school_choice(self.wealth, self.location, self.priorities)
         
-        # TO DO: Does this really matter? It isn't so much the same 
-        # family choosing again and again. Most families make these sorts of choices
-        # (boarding school, college) 1-5x per lifetime. So keeping track of their 
-        # decreasing wealth as a result of 1 payment is really not that important.
-
-        # Maybe instead at each step we just randomly generate new families each time
+        # TO DO: Does this really matter? E.g. do we want a family agent who persists?
+        # It isn't so much the same family choosing again and again. 
+        # Most families make these sorts of choices (boarding school, college)
+         # 1-5x per lifetime. So keeping track of their decreasing wealth 
+         # as a result of 1 payment is really not that important.
+        # Maybe instead, at each step we just randomly generate new families each time
         # and the random parameters we initialize maybe change over time as families
-        # generally change. 
+        # generally change. TO DO: How do these parameters change?
+
         # Give the chosen school some money.
         self.model.schedule.agents[chosen_school].endowment += self.model.schedule.agents[chosen_school].tuition
-        # Take the tuition payment from this agent.
-        self.wealth -= self.model.schedule.agents[chosen_school].tuition
+        # Take the tuition payment from this agent. See above: not really relevant I don't think?
+        # self.wealth -= self.model.schedule.agents[chosen_school].tuition
+
+        # If the school is better than expected, future prestige goes up. Vice versa.
+        # Maybe this makes families in future more open to low prestige schools, as
+        # reputation spreads that schools with lower prestige can still provide good service?
+        gap = (self.model.schedule.agents[chosen_school].efficacy - self.model.schedule.agents[chosen_school].prestige)
+        if gap >= 0:
+            self.model.schedule.agents[chosen_school].prestige += .5 * gap
+        else:
+            self.model.schedule.agents[chosen_school].prestige -= .5 * gap
 
 class SchoolAgent(Agent):
     def __init__(self, unique_id, model, 
@@ -97,12 +105,16 @@ class SchoolAgent(Agent):
         self.endowment = self.endowment * (1 + ENDOWMENT_GROWTH_RATE)
 
         # Prestige can fall due to exongenous event (eg. sex scandal). 
+        # TO DO: Make scandals of varying size and probability.
         possible_prestiges = [self.prestige, self.prestige*.75]
         draw = np.random.choice(possible_prestiges, 1, p=[1-SCANDAL_PROBABILITY, SCANDAL_PROBABILITY])
-        if possible_prestiges.index(draw) == 1:
-            print('School {} had a sex scandal! Old prestige was {} and new prestige is {}.'
-                .format(self.unique_id, self.prestige, self.prestige*.75))
+        # if possible_prestiges.index(draw) == 1:
+            # print('School {} had a sex scandal! Old prestige was {} and new prestige is {}.'
+                # .format(self.unique_id, self.prestige, self.prestige*.75))
         self.prestige = draw[0] 
+
+        print('family_priority_weights', self.model.family_priority_weights)
+        self.model.family_priority_weights = [i*.99 for i in self.model.family_priority_weights]
 
         return
 
--- model.py
+++ model.py
@@ -35,37 +35,59 @@ import helpers
 # ______________________________________________________________________________
 # Constants
 
-POSSIBLE_PRIORITIES = {'prestige':.6, 'cost':.4}
-NUM_SIMULATIONS = 1
-NUM_TIME_STEPS = 1
-NUM_FAMILIES = 5
-NUM_SCHOOLS = 3
+POSSIBLE_PRIORITIES     = {'prestige':.6, 'cost':.4}
+FAMILY_PRIORITY_WEIGHTS = [.3, .3, .3]
+MAX_ENDOWMENT_DRAW      = .1  # Most schools draw ~5%. Drawing more than ENDOWMENT_GROWTH_RATE should decrease endowment over time.
+NUM_SIMULATIONS         = 1
+NUM_TIME_STEPS          = 5
+NUM_FAMILIES            = 5
+NUM_SCHOOLS             = 3
 
 # ______________________________________________________________________________
 # Model
 
 class SchoolModel(Model):
-    """
-    Docstring.
-    """
-    def __init__(self, num_families, num_schools):
-        self.num_agents  = num_families
-        self.num_schools = num_schools
+    """A mode to simulate family-school choice.
 
-        self.schedule    = RandomActivation(self)  # How to take steps -- each agent activeated in random order. 
+    A outlined description is as follows:
+    1. Initialize `num_schools` # of schools with give parameters.
+    2. At each step (a year), we generate `num_families` # of families.
+    Each family has a set of priorities and then makes a school choice.
+    3. After choosing a school, the school finances and future family 
+    decision priorities evolve.
+    """
+    def __init__(self, num_families, num_schools, family_priority_weights):
+        self.num_families              = num_families
+        self.num_schools               = num_schools
+        # Though we randomly initialize family priorities, this global variable
+        # controls macro market preferences. E.g. are families growing more 
+        # cost conscious? TO DO: Make this happen as general macro-economy worsens,
+        # which should also lower school endowment growth rate. 
+        self.family_priority_weights   = family_priority_weights
+
+        # How to take steps -- each agent activeated in random order. 
+        self.schedule    = RandomActivation(self)  
 
         # Create schools
         for i in range(self.num_schools):
-            endowment      = random.randint(0,100)
-            prestige       = random.randint(0,100)
+            # Parameterize school.
+            # TO DO: Implement in a seperate function?
+            endowment      = scipy.stats.expon().rvs(1)[0] #random.randint(0,100)
+            prestige       = np.abs(np.random.normal(0, 1, 1)[0])
             tuition        = (prestige / 2) # Cost is directly related to prestige.
             annual_fund    = random.randint(0,100)
-            efficacy       = random.randint(0,100)
-            endowment_draw = random.randint(0,100)
-
+            efficacy       = np.abs(np.random.normal(0, 1, 1)[0])
             priorities     = helpers.make_random_school_priorities()
-            
-            location       = np.random.normal(0, 100, 1)[0] # Set a school in city close to 0, remote farther from there.
+            # Endowment draw is 0 if the school only cares about prestige,
+            # whereas endownment draw is MAX_ENDOWMENT_DRAW 
+            # if school cares only about equity and efficacy. Theory: schools that
+            # care about equity and efficacy will spend on current students, whereas 
+            # per Gladwell, saving money and building endowment increases reputation
+            # at a cost of not adding much immediate value to today's students.
+            endowment_draw = MAX_ENDOWMENT_DRAW * (priorities['Equity'] + priorities['Efficacy']) / 1
+            # Set a school in city close to 0, remote farther from there. Normal distribution implies
+            # more schools closer to 1D center.
+            location       = np.random.normal(0, 100, 1)[0]
 
             a = agents.SchoolAgent(i, self, 
                             endowment,  
@@ -78,8 +100,19 @@ class SchoolModel(Model):
                             location)
             self.schedule.add(a)
 
-        # Parents
-        for j in range(self.num_schools, self.num_agents + self.num_schools): # Need to have unique ids.
+    def step(self, i):
+        # schools= [obj for obj in self.schedule._agents.values() if isinstance(obj, agents.SchoolAgent)]
+        # prestiges = [i.prestige for i in schools]
+
+
+        # Each step, create new families.
+        # print(i)
+        start = self.num_schools  + i * self.num_families
+        end   = self.num_schools  + (i + 1) * self.num_families #(self.num_schools  + self.num_families) * (i+1)
+        # print(self.num_families)
+        # print('start', 'end', start, end)
+
+        for j in range(start, end): # Need to have unique ids.
 
             # A randomly initialized wealth. As per https://arxiv.org/pdf/cond-mat/0103544,
             # most American's have wealth following exponential distribution.
@@ -89,34 +122,40 @@ class SchoolModel(Model):
             
             # What is their top priority? TO DO: Make priorities a weighted average where
             # they care about all priorities but individual families have different weightings.
-            priority = random.choice(list(POSSIBLE_PRIORITIES.keys()))
+            priorities = helpers.make_random_family_priorities()
+            # print('Family {} has these priorities {}'.format(j, priority))
             # Where they live. Most people live in median place. This is simulating
             # cities versus rural living.
             location = np.random.normal(0, 100, 1)[0] # Set a school in city close to 0, remote farther from there.
             # TO DO, have schools choose or set location based on some more intelligent game theoretic model.
 
-            parent   = agents.FamilyAgent(j, self, wealth, priority, location)
-            self.schedule.add(parent)
-
-    def step(self):
-        schools= [obj for obj in self.schedule._agents.values() if isinstance(obj, agents.SchoolAgent)]
-        prestiges = [i.prestige for i in schools]
+            family   = agents.FamilyAgent(j, self, wealth, priorities, location)
+            self.schedule.add(family)
+        # start = end + 1
+        # end   = end + self.num_families
         self.schedule.step()
+        current_families = [obj for obj in self.schedule._agents.values() if isinstance(obj, agents.FamilyAgent)]
+        print([i.unique_id for i in current_families])
+        for f in current_families:
+             self.schedule.remove(f) 
 
 # ______________________________________________________________________________
 # Run model.
 
 if __name__=='__main__':
-    model = SchoolModel(num_families = NUM_FAMILIES, num_schools = NUM_SCHOOLS)
-
     for _ in range(NUM_SIMULATIONS):
-        model = SchoolModel(num_families=NUM_FAMILIES, num_schools=NUM_SCHOOLS)
+        model = SchoolModel(num_families=NUM_FAMILIES, 
+                            num_schools=NUM_SCHOOLS, 
+                            family_priority_weights=FAMILY_PRIORITY_WEIGHTS)
+        
         helpers.initial_inspection(model)
+        
         for i in range(NUM_TIME_STEPS): # How many years to let this run. 
-            # print('Step {}'.format(i))
-            model.step()
+            print('Step {}'.format(i))
+            model.step(i)
 
-    helpers.retrospective(model)
+        helpers.retrospective(model)
+    helpers.simulation_retrospective()
 
 
 
@@ -168,4 +207,4 @@ if __name__=='__main__':
 # print(DG.number_of_edges())
 # empty_model = MoneyModel(10)
 # empty_model.step()
-# print(empty_model.num_agents)
+# print(empty_model.num_families)
